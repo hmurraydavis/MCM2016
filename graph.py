08e53501438b7498a14457b6a85a4604fc8db301
Halie Murray-Davis <hmurraydavis@yahoo.com>
2016-01-29 16:14:01 -0500

    Vertex colors are pretty and opaque

--- graph.py
+++ graph.py
@@ -16,27 +16,31 @@ for key in countryKeys:
     graph.vs[countryDict[key]]['label'] = key
     graph.vs[countryDict[key]]['p_death'] = 4
 
+graph.vs['label_size'] = 40 ## Set fount size
+graph.vs['shape'] = 'circle'
+
 ## Add edges for borders and boats:
 graph.add_edges( contiguousBorders )
 graph.es[0:len(contiguousBorders)]["color"] = "yellow"
 graph.add_edges( waterRoutes )
 
 ## Color countries by geographical region:
+vertexOpacity = .5
 for vertexNumber in range(len(countryDict)):
     if graph.vs[vertexNumber]['label'] == 'Syria':
-        graph.vs[vertexNumber]['color'] = 'red'
+        graph.vs[vertexNumber]['color'] = 'rgba(78,0,18, '+str(vertexOpacity)+')'
     elif graph.vs[vertexNumber]['label'] in COS.africaCountries():
-        graph.vs[vertexNumber]['color'] = 'orange'
+        graph.vs[vertexNumber]['color'] = 'rgba(156, 52, 76, '+str(vertexOpacity)+')'
     elif graph.vs[vertexNumber]['label'] in COS.middleEastCountries():
-        graph.vs[vertexNumber]['color'] = 'yellow' 
+        graph.vs[vertexNumber]['color'] = 'rgba(170, 95, 57, '+str(vertexOpacity)+')'
     elif graph.vs[vertexNumber]['label'] in COS.centralEuropeCountries():
-        graph.vs[vertexNumber]['color'] = 'blue' 
+        graph.vs[vertexNumber]['color'] = 'rgba(0,0,100, '+str(vertexOpacity)+')' 
     elif graph.vs[vertexNumber]['label'] in COS.southernEuropeCountries():
-        graph.vs[vertexNumber]['color'] = 'pink' 
+        graph.vs[vertexNumber]['color'] = 'rgba(38, 113, 88, '+str(vertexOpacity)+')'
     elif graph.vs[vertexNumber]['label'] in COS.westernEuropeCountries():
-        graph.vs[vertexNumber]['color'] = 'purple' 
+        graph.vs[vertexNumber]['color'] = 'rgba(87,49,50, '+str(vertexOpacity)+')'
     elif graph.vs[vertexNumber]['label'] in COS.nordicCountries():
-        graph.vs[vertexNumber]['color'] = 'green' 
+        graph.vs[vertexNumber]['color'] = 'rgba(0,57,38, '+str(vertexOpacity)+')'
     else:
         print 'WARNING: No color specified for: ',graph.vs[vertexNumber]['label']
 
@@ -47,7 +51,7 @@ layout = graph.layout("auto")
 
 visual_style = { "edge_width":6,
     "vertex_size": 95, 
-    'bbox':(0, 0, 1300, 1300),
+    'bbox':(0, 0, 2300, 2300),
     'layout': layout,
     'margin': 68 }
 
