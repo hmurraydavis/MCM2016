6ba7a77ba09af7aa635cfaa1ce095f539a0b92d8
Halie Murray-Davis <halie@localhost.localdomain>
2016-01-29 15:22:36 -0500

    Add country coloring by UN geographical region https://en.wikipedia.org/wiki/Eastern_Europe#/media/File:Europe_subregion_map_UN_geoscheme.svg

--- graph.py
+++ graph.py
@@ -21,8 +21,35 @@ graph.add_edges( contiguousBorders )
 graph.es[0:len(contiguousBorders)]["color"] = "yellow"
 graph.add_edges( waterRoutes )
 
+## Color countries by geographical region:
+for vertexNumber in range(len(countryDict)):
+    if graph.vs[vertexNumber]['label'] == 'Syria':
+        graph.vs[vertexNumber]['color'] = 'red'
+    elif graph.vs[vertexNumber]['label'] in COS.africaCountries():
+        graph.vs[vertexNumber]['color'] = 'orange'
+    elif graph.vs[vertexNumber]['label'] in COS.middleEastCountries():
+        graph.vs[vertexNumber]['color'] = 'yellow' 
+    elif graph.vs[vertexNumber]['label'] in COS.centralEuropeCountries():
+        graph.vs[vertexNumber]['color'] = 'blue' 
+    elif graph.vs[vertexNumber]['label'] in COS.southernEuropeCountries():
+        graph.vs[vertexNumber]['color'] = 'pink' 
+    elif graph.vs[vertexNumber]['label'] in COS.westernEuropeCountries():
+        graph.vs[vertexNumber]['color'] = 'purple' 
+    elif graph.vs[vertexNumber]['label'] in COS.nordicCountries():
+        graph.vs[vertexNumber]['color'] = 'green' 
+    else:
+        print 'WARNING: No color specified for: ',graph.vs[vertexNumber]['label']
 
+
+graph.es['weight'] = range(0,40)
+
+layout = graph.layout("auto")
+
+visual_style = { "edge_width":6,
+    "vertex_size": 95, 
+    'bbox':(0, 0, 1300, 1300),
+    'layout': layout }
 
 #layout = graph.layout("kamada_kawai")
-layout = graph.layout("auto")
-igraph.plot(graph, bbox=(0, 0, 1000, 1000), layout = layout)
+
+igraph.plot(graph, **visual_style)
